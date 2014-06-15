d5861eab6b12f6156010be898ae3a46ba2b08fe7
PATSY WALSH^C <PATSYPATSYPATSY@gmail.com^C>
2014-06-15 14:27:46 -0400

    movie app commit msg

---
+++ _Patsy_Walsh/ex_apartment_objects/lib/building.rb
@@ -0,0 +1,8 @@
+#Building Class
+class Building
+	attr_accessor :building_name, :building_address
+	def initialize(building_name, building_address)
+		@building_name = building_name
+		@building_address = building_address
+	end
+end
